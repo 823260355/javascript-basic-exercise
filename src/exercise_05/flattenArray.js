eddd53dd6a828949bae2e67a2a4aae95fde70437
LiYuan1229 <823260355@qq.com>
2020-09-09 16:25:37 +0800

    finish exercise_05/flattenArray.js

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,21 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  if (array.length === 0) {
+    return [];
+  }
+  const arrResult = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j += 1) {
+        arrResult.push(array[i][j]);
+      }
+    } else {
+      arrResult.push(array[i]);
+    }
+  }
+  return arrResult;
 }
